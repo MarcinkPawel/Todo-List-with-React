f8cca288b0dccdab62f6288fb9d6f33870b4960a
Pawel Marcinkowski <marcinkowski.pwl@gmail.com>
2022-11-27 20:40:35 +0100

    adding empty task blocked

--- src/Form/index.js
+++ src/Form/index.js
@@ -6,8 +6,11 @@ const Form = ({ addNewTask }) => {
 
   const onFormSubmit = (event) => {
     event.preventDefault();
+    if (newTaskContent === "") {
+      return;
+    }
     addNewTask(newTaskContent.trim());
-    setNewTaskContent("")
+    setNewTaskContent("");
   };
 
   return (
